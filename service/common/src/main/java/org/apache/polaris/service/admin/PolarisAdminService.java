3aedaaf7d163a82d64f55c947a8270082bbc57cc
gfakbar20 <gfakbar20@gmail.com>
2025-06-30 19:48:34 +0700

    backup commit: main source code
    # Conflicts:
    #	polaris-core/src/main/java/org/apache/polaris/core/entity/CatalogEntity.java
    #	service/common/src/main/java/org/apache/polaris/service/admin/PolarisAdminService.java

--- service/common/src/main/java/org/apache/polaris/service/admin/PolarisAdminService.java
+++ service/common/src/main/java/org/apache/polaris/service/admin/PolarisAdminService.java
@@ -19,6 +19,7 @@
 package org.apache.polaris.service.admin;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static org.apache.polaris.core.entity.EntityConverter.toNamespace;
 import static com.google.common.base.Preconditions.checkState;
 
 import jakarta.annotation.Nonnull;
